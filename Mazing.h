6b2c24b89b740e22c088fa45862c71ff67206edd
MinecraftDawn <eric4902077@gmail.com>
2018-10-23 00:04:12 +0800

    Initial commit

---
+++ Mazing.h
@@ -0,0 +1,14 @@
+//
+// Created by Eric on 2018/10/23.
+//
+
+#ifndef MAZING_MAZING_H
+#define MAZING_MAZING_H
+
+
+class Mazing {
+
+};
+
+
+#endif //MAZING_MAZING_H
---
+++ Path.h
@@ -0,0 +1,14 @@
+//
+// Created by Eric on 2018/10/23.
+//
+
+#ifndef MAZING_PATH_H
+#define MAZING_PATH_H
+
+
+class Path {
+
+};
+
+
+#endif //MAZING_PATH_H
